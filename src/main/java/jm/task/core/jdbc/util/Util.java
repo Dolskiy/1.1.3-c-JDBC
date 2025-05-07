40d8473cd1e6e733c2aa9799d4c01da619627d19
Dolskiy <imperox@ro.ru>
2025-05-08 00:09:59 +0300

    Задача 1.1.3 выполнена.

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private static final String HOSTNAME = "127.0.0.1";
+    private static final String DBNAME = "mydatabase";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getMyConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+
+        String connectionURL = "jdbc:mysql://" + HOSTNAME + ":3306/" + DBNAME;
+
+        Connection conn = DriverManager.getConnection(connectionURL, USERNAME, PASSWORD);
+        return conn;
+    }
+
 }
